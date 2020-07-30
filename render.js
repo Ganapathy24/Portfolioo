a056717359227ed91624d9d34df52f1d67dbe915
ganapathy24 <techiiinet@gmail.com>
2020-07-30 11:30:13 +0530

    60%

--- render.js
+++ render.js
@@ -15,3 +15,38 @@ function render(event,topic){
     }
     
 document.getElementById("default").click();
+
+function renderCertificates(event,platform){
+    var i,tabcontent,tablinks;
+    tabcontent = document.getElementsByClassName('certificate');
+
+    for(i=0;i<tabcontent.length;i++)
+    {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("btns");
+    for(i = 0;i<tablinks.length;i++){
+        tablinks[i].className = tablinks[i].className.replace("active","");
+    }
+    for(i=1;i<=8;i++){
+    document.getElementById(platform+""+i).style.display = "block";
+    }
+      event.currentTarget.className += " active";
+}
+document.getElementById('def').click();
+function renderIntern(event,platform){
+    var i,tabcontent,tablinks;
+    tabcontent = document.getElementsByClassName('intern');
+
+    for(i=0;i<tabcontent.length;i++)
+    {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("btns");
+    for(i = 0;i<tablinks.length;i++){
+        tablinks[i].className = tablinks[i].className.replace("active","");
+    }
+    document.getElementById(platform).style.display = "block";
+    event.currentTarget.className += " active";
+}
+document.getElementById("deff").click();
